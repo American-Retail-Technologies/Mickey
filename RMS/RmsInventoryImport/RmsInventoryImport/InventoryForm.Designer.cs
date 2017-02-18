53df7e452435db92e111dc010f8ea6dad43fe39a
Surender Mohan <smohan36@gmail.com>
2017-02-18 09:39:50 -0800

    RmsInventoryImport project

---
+++ RMS/RmsInventoryImport/RmsInventoryImport/InventoryForm.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace RmsInventoryImport
+{
+    partial class InventoryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dgCsvFile = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.chkFirstRowHeader = new System.Windows.Forms.CheckBox();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnRollback = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgCsvFile)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 24);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(575, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 393);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(575, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "Welcome to ATR item invetory importer....";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.helpToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(575, 24);
+            this.menuStrip2.TabIndex = 2;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
+            this.toolStripMenuItem1.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // dgCsvFile
+            // 
+            this.dgCsvFile.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgCsvFile.Location = new System.Drawing.Point(13, 42);
+            this.dgCsvFile.Name = "dgCsvFile";
+            this.dgCsvFile.Size = new System.Drawing.Size(550, 150);
+            this.dgCsvFile.TabIndex = 3;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // chkFirstRowHeader
+            // 
+            this.chkFirstRowHeader.AutoSize = true;
+            this.chkFirstRowHeader.Location = new System.Drawing.Point(13, 24);
+            this.chkFirstRowHeader.Name = "chkFirstRowHeader";
+            this.chkFirstRowHeader.Size = new System.Drawing.Size(117, 17);
+            this.chkFirstRowHeader.TabIndex = 4;
+            this.chkFirstRowHeader.Text = "First row is header?";
+            this.chkFirstRowHeader.UseVisualStyleBackColor = true;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(223, 17);
+            this.toolStripStatusLabel1.Text = "Welcome to ART inventory import tool....";
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(488, 198);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(75, 23);
+            this.btnImport.TabIndex = 5;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // btnRollback
+            // 
+            this.btnRollback.Location = new System.Drawing.Point(13, 199);
+            this.btnRollback.Name = "btnRollback";
+            this.btnRollback.Size = new System.Drawing.Size(75, 23);
+            this.btnRollback.TabIndex = 6;
+            this.btnRollback.Text = "Rollback";
+            this.btnRollback.UseVisualStyleBackColor = true;
+            this.btnRollback.Click += new System.EventHandler(this.btnRollback_Click);
+            // 
+            // InventoryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 415);
+            this.Controls.Add(this.btnRollback);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.chkFirstRowHeader);
+            this.Controls.Add(this.dgCsvFile);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+            this.Name = "InventoryForm";
+            this.Text = "American Retail Tech RMS Importer";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgCsvFile)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dgCsvFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.CheckBox chkFirstRowHeader;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnRollback;
+    }
+}
+
---
+++ RMS/RmsInventoryImport/RmsInventoryImport/InventoryForm.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RmsInventoryImport
+{
+    public partial class InventoryForm : Form
+    {
+        CsvParser csvParser = new CsvParser();
+        CsvParser csvHeaderParser = new CsvParser();
+        IEnumerable<string[]> csvHeaders;
+        IEnumerable<string[]> csvRecords;
+
+        public InventoryForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateDataGridViewHeader(DataGridView dgv, String textLine, bool fIsHeader)
+        {
+            csvHeaders = csvHeaderParser.GetCsvRecords(textLine, false);
+            foreach (string[] strArr in csvHeaders)
+            {
+                for (int i = 0; i < strArr.Length; i++)
+                {
+                    if (fIsHeader)
+                    {
+                        dgv.Columns.Add(strArr[i], strArr[i]);
+                    }
+                    else
+                    {
+                        dgv.Columns.Add(i.ToString(), i.ToString());
+                    }
+                }
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int size = -1;
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                String file = openFileDialog1.FileName;
+                try
+                {
+                    String text = File.ReadAllText(file);
+                    size = text.Length;
+                    // Create the header row
+                    string firstline = text.Substring(0, text.IndexOf("\n"));
+                    CreateDataGridViewHeader(dgCsvFile, firstline, chkFirstRowHeader.Checked);
+
+                    csvRecords = csvParser.GetCsvRecords(text, chkFirstRowHeader.Checked);
+                    toolStripStatusLabel1.Text = "Read " + csvRecords.Count().ToString() + " lines from file " + file + ".....";
+                    statusStrip1.Update();
+                    DataGridViewRow dgvr = null;
+                    foreach (string[] strArr in csvRecords)
+                    {
+                        dgvr = new DataGridViewRow();
+                        dgvr.CreateCells(dgCsvFile, strArr);
+                        dgCsvFile.Rows.Add(dgvr);
+                    }
+                    dgCsvFile.Show();
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRollback_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
