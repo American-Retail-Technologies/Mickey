29ff9f43c16417bf13c4e0c867a1b933fdccad5b
TenzinNKhando <tkhando@pioneer-inc.com>
2017-07-07 14:40:17 -0700

    Breadcrumb Update: Apply the affect to list greater than 2, otherwise hide it.

--- Themes/ars-theme/app/design/frontend/Sm/market/Sm_Market/web/js/artjs.js
+++ Themes/ars-theme/app/design/frontend/Sm/market/Sm_Market/web/js/artjs.js
@@ -1,28 +1,30 @@
 define( [ "jquery" ], function ( $ ) {
 	"use strict";
-	
 	//Reduce the number of breadcrumbs if there is more than 4 categories
 	function reduceBreadCrumb(){
-		var $totalBreadcrumb = $('.breadcrumbs ul li').length > 3 ? $('.breadcrumbs ul li').length : false;
+		var $totalBreadcrumb = $('.breadcrumbs ul li').length;
 		
-		if($totalBreadcrumb){
-		  var $breadcrumb = $('.breadcrumbs ul li');
-
-		  $.each($breadcrumb, function(index, elem) {
-			if( 0 < index && index < ($totalBreadcrumb - 2)){
-			  $(elem).find('a').text('...');;
-			}else if( index == ($totalBreadcrumb - 2)){
-			  //Previous category can only hold 22 characters for iphone6
-			  //substring and add '...' to the end if greater than
-			  var $previousCategory = $(elem).find('a').text().length > 22 ? ($(elem).find('a').text().substring(0, 19) + '...') : $(elem).find('a').text();
-			  $(elem).find('a').text($previousCategory);
-			}
-			else if( index == $totalBreadcrumb -1 ){
-			  $(elem).css('display', 'none');
-			}
-		  });
+		//Level 1
+		if($totalBreadcrumb == 2){
+			$('.breadcrumbs').css('display', 'none');
+		}else { //Level >2
+			var $breadcrumb = $('.breadcrumbs ul li');
+			$.each($breadcrumb, function(index, elem){
+				if( 0 < index && index < ($totalBreadcrumb - 2)){
+				  $(elem).find('a').text('...');;
+				}else if( index == ($totalBreadcrumb - 2)){
+				  //Previous category can only hold 22 characters for iphone6
+				  //if greater than substring and add '...' to the end 
+				  var $previousCategory = $(elem).find('a').text().length > 22 ? ($(elem).find('a').text().substring(0, 19) + '...') : $(elem).find('a').text();
+				  $(elem).find('a').text($previousCategory);
+				}
+				else if( index == $totalBreadcrumb -1 ){
+				  $(elem).css('display', 'none');
+				}
+			});
 		}
 	}
+	
 	$( document ).ready(function() {
 		
 		//https://stackoverflow.com/questions/9720294/jquery-how-to-detect-window-width-on-the-fly
