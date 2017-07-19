7e587abb6586264dea6060c8653ae2a79cf46e7d
Tenzin Khando <tenzin@americanretailsupply.com>
2017-07-19 13:19:39 -0700

    Customer List script: Moved file from phpimports/ to art-page/

--- Themes/art-page/new_customer.php
+++ Themes/art-page/new_customer.php
@@ -1,7 +1,7 @@
 <?php
 //https://stackoverflow.com/questions/5004233/jquery-ajax-post-example-with-php?noredirect=1&lq=1
 
-$connect = new mysqli("ars-mysql.crymzjqricqv.us-west-2.rds.amazonaws.com","ars_dbroot","American1","ars_staging_magento1");
+$connect = mysqli_connect("ars-mysql.crymzjqricqv.us-west-2.rds.amazonaws.com","ars_dbroot","American1","ars_staging_magento1");
 //$customers = "SELECT * FROM customer_entity ORDER BY created_at DESC";
 if($_POST["fromDate"]){
 	if (mysqli_connect_errno()) {
@@ -11,7 +11,7 @@ if($_POST["fromDate"]){
 	$customers = "SELECT email, firstname, lastname FROM `customer_entity` WHERE `created_at` >= '".$_POST["fromDate"]."' ORDER BY `created_at` DESC";
 	//echo $customers;
 	
-	$queryResult = $connect->query($customers);
+	$queryResult = mysqli_query($connect, $customers);
 	//$result = $queryResult->fetch_all();
 	while($result = $queryResult->fetch_row()){
 		echo json_encode($result);
