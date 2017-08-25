5821051755d78b57f02ceb2854d21a8a46c6e0a9
TenzinNKhando <tkhando@pioneer-inc.com>
2017-08-24 17:27:45 -0700

    JS: Removed Code for turning LTL price to 0 in art-js file

--- Themes/ars-theme/app/design/frontend/Sm/market/Sm_Market/web/js/artjs.js
+++ Themes/ars-theme/app/design/frontend/Sm/market/Sm_Market/web/js/artjs.js
@@ -1,6 +1,5 @@
 define( [ "jquery" ], function ( $ ) {
 	"use strict";
-	console.log("ART JS FILE");
 	//Reduce the number of breadcrumbs if there is more than 4 categories
 	function reduceBreadCrumb(){
 		var $totalBreadcrumb = $('.breadcrumbs ul li').length;
@@ -26,39 +25,7 @@ define( [ "jquery" ], function ( $ ) {
 		}
 	}
 	
-	function changeLTLShippingPrice(){
-		console.log("changingLTLPrice function");
-		if( $("#co-shipping-method-form")[0]){
-			$(".field.choice.item [for='s_method_shipping']").text('LTL/ Freight Quote');
-		}
-		if( $(".data.table.totals")[0]){
-			var $shippingMethod = $(".data.table.totals .totals.shipping.excl .mark .value").text();
-			if($shippingMethod === "(Shipping Will Be Quoted Separately - LTL/ Freight Quote)"){
-				$(".totals.shipping.excl .amount .price").text('Quoted Seperately');
-			}
-		}
-
-		if($(".table-checkout-shipping-method")[0]){
-			var $shippingMethod = $(".table-checkout-shipping-method #label_method_shipping_shipping").text();
-
-			if($shippingMethod === "LTL/ Freight Quote"){
-				//changed price to new text
-				$(".table-checkout-shipping-method .price .price").text('Quoted Seperately');
-			}
-
-		}
-		if($(".data.table.table-totals")[0]){
-			var $shippingMethod = $(".data.table.table-totals .totals.shipping.excl .value").text();
-
-			if($shippingMethod === "Shipping Will Be Quoted Separately - LTL/ Freight Quote"){
-				//changed price to new text
-				$(".data.table.table-totals .totals.shipping.excl .amount .price").text('Quoted Seperately');
-			}
-		}
-	}
-	
 	$( document ).ready(function() {
-		changeLTLShippingPrice();
 		//https://stackoverflow.com/questions/9720294/jquery-how-to-detect-window-width-on-the-fly
 		var $window = $(window);
 		
