c66d4b730888dc450749e26ea6441064a44aca55
Surender Mohan <smohan36@gmail.com>
2017-09-12 18:20:25 -0700

    add missing files

---
+++ Themes/scripts/env.staging.php
@@ -0,0 +1,71 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin',
+  ),
+  'crypt' => 
+  array (
+    'key' => '0184b529143c2dcfdae667bd66f24a69',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'ars-mysql.crymzjqricqv.us-west-2.rds.amazonaws.com',
+        'dbname' => 'ars_staging_magento1',
+        'username' => 'ars_dbroot',
+        'password' => 'American1',
+        'model' => 'mysql4',
+        'engine' => 'innodb',
+        'initStatements' => 'SET NAMES utf8;',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'production',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'full_page' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+    'compiled_config' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Sat, 11 Feb 2017 20:09:34 +0000',
+  ),
+  'http_cache_hosts' => 
+  array (
+    0 => 
+    array (
+      'host' => '127.0.0.1',
+      'port' => '80',
+    ),
+  ),
+);
